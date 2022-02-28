8d8f447599562cf51601ff0fdebb1949f7ee10a4
molly-practice <80125646+molly-practice@users.noreply.github.com>
2022-02-27 17:14:54 -0800

    default prop values- see bottom of src/app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ class IndecisionApp extends React.Component {
     this.handleAddOption = this.handleAddOption.bind(this);
     this.handlePick = this.handlePick.bind(this);
     this.state = {
-      options: []
+      options: props.options
     };
   }
 
@@ -46,12 +46,11 @@ class IndecisionApp extends React.Component {
   }
 
   render() {
-    const title = 'Indecision';
     const subTitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
-      <Header title={title} subTitle={subTitle}/>
+      <Header subTitle={subTitle}/>
       <Action 
         hasOptions={this.state.options.length > 0}
         handlePick={this.handlePick}
@@ -68,13 +67,21 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+  options: []
+}
+
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subTitle}</h2>
+      {props.subTitle && <h2>{props.subTitle}</h2>}
     </div>
   );
+};
+
+Header.defaultProps = {
+  title: 'Indecision'
 };
 
 const Action = (props) => {
@@ -143,21 +150,4 @@ class AddOption extends React.Component {
   }
 }
 
-// stateless functional component example. they are faster than regular components!
-// if a component doesn't have a state, you can make it a stateless functional component
-// it can't have a state, but you *can* pass it props
-// Remember, these s.f.components (and also arrow functions) can't access 'this'
-
-// you pass in 'props' which is just treated like this.props (it's just an object), and then you access data in it with props.whatever
-// const User = (props) => {
-//   return (
-//     <div>
-//       <p>Name: {props.name}</p>
-//       <p>Age: {props.age}</p>
-//     </div>
-//   )
-// }
-
-// here's how you pass props to a s.f.component 
-// ReactDOM.render(<User name="Molly" age={30}/>, document.getElementById('app'));
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp options={['Option one', 'Option two']}/>, document.getElementById('app'));
