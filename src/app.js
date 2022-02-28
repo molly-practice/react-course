886b70a27b522aa262091318506cbb4aee5ee07b
molly-practice <80125646+molly-practice@users.noreply.github.com>
2022-02-27 17:05:47 -0800

    converting some components to stateless functional components, with notes

--- src/app.js
+++ src/app.js
@@ -68,55 +68,44 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-// react components require the render method to be defined
-render() {
-  // render returns some jsx
+const Header = (props) => {
   return (
     <div>
-      <h1>{this.props.title}</h1>
-      <h2>{this.props.subTitle}</h2>
+      <h1>{props.title}</h1>
+      <h2>{props.subTitle}</h2>
     </div>
   );
-}
-}
+};
 
-class Action extends React.Component {
-  
-  render() {
-    return (
-      <div>
-        <button 
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOptions}
-        >
-          What should I do?
-        </button>
-      </div>
-    );
-  }
-}
-
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-        {this.props.options.map((option) => <Option key={option} optionText={option} />)}
-      </div>
-    );
-  }
-}
-
-class Option extends React.Component {
-  render() {
-    return (
+const Action = (props) => {
+  return (
     <div>
-      {this.props.optionText}
+      <button 
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        What should I do?
+      </button>
     </div>
-    );
-  }
-}
+  );
+};
+
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.map((option) => <Option key={option} optionText={option} />)}
+    </div>
+  );
+};
+
+const Option = (props) => {
+  return (
+    <div>
+      {props.optionText}
+    </div>
+  );
+};
 
 class AddOption extends React.Component {
   // now that we're using 'this' inside of one of our functions, we should set up the constructor function  so we can bind it
@@ -154,4 +143,21 @@ class AddOption extends React.Component {
   }
 }
 
+// stateless functional component example. they are faster than regular components!
+// if a component doesn't have a state, you can make it a stateless functional component
+// it can't have a state, but you *can* pass it props
+// Remember, these s.f.components (and also arrow functions) can't access 'this'
+
+// you pass in 'props' which is just treated like this.props (it's just an object), and then you access data in it with props.whatever
+// const User = (props) => {
+//   return (
+//     <div>
+//       <p>Name: {props.name}</p>
+//       <p>Age: {props.age}</p>
+//     </div>
+//   )
+// }
+
+// here's how you pass props to a s.f.component 
+// ReactDOM.render(<User name="Molly" age={30}/>, document.getElementById('app'));
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
